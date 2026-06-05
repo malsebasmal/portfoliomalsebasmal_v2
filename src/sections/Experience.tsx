ec6ba711216c0e3cc1397221a0d2958ae081d10c
malsebasmal <antoniomaldonadoestudio@gmail.com>
2026-06-05 13:03:50 -0500

    feat: responsive mobile

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -6,7 +6,9 @@ export default function Experience() {
   return (
     <section className="my-8">
       <div className="w-full flex items-center justify-center gap-4 mb-6">
-        <h3 className="text-3xl font-bold">Professional Experience</h3>
+        <h3 className="text-2xl md:text-3xl font-bold">
+          Professional Experience
+        </h3>
         <div className="border-[0.5px] border-solid border-neutral-700 flex-1" />
       </div>
       <FadeInGrid cols={3} gap={6} delay={100}>
